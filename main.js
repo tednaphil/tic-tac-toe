3b98b22d7dd41546a5df415148b4cd60befa2399
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-09 16:52:16 -0600

    Add query selectors

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // QUERY SELECTORS
+var playerOneWins = document.querySelector('#player-one-wins');
+var playerTwoWins = document.querySelector('#player-two-wins');
+var announcement = document.querySelector('#announcement')
+var gameBoard = document.querySelector('.game-board');
+var boardSpaces = {
+    0: document.querySelector('#position-0'),
+    1: document.querySelector('#position-1'),
+    2: document.querySelector('#position-1'),
+    3: document.querySelector('#position-1'),
+    4: document.querySelector('#position-1'),
+    5: document.querySelector('#position-1'),
+    6: document.querySelector('#position-1'),
+    7: document.querySelector('#position-1'),
+    8: document.querySelector('#position-1')
+}
 
 // GLOBAL VARIABLES
 
