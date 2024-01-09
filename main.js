609fe1100bfb8ce60dbe4374bdb347f2dbd0aab4
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-09 16:12:57 -0600

    Add pseudocode structure

---
+++ main.js
@@ -0,0 +1,17 @@
+// QUERY SELECTORS
+
+// GLOBAL VARIABLES
+
+// EVENT LISTENERS
+
+// FUNCTIONS
+// Create a player
+// Increase Wins
+// Keep track of gameboard data
+// Keep track of which player's turn it is
+// Check board for win conditions
+// Check board for a draw
+// Reset gameboard and start new game
+// Display gameboard data
+// Display player data
+
