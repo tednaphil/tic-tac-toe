24cd28857ffa13089696d70abaee59c46d83f8c9
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-09 18:23:42 -0600

    Pseudocode data model and DOM functions

--- main.js
+++ main.js
@@ -17,9 +17,31 @@ var boardSpaces = {
 
 // GLOBAL VARIABLES
 var players = [];
+var winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+var gameBoardMoves = {
+    playerOneMoves: [],
+    playerTwoMoves: []
+};
+var turns = 0
 
 // EVENT LISTENERS
 addEventListener('load', loadGame);
+gameBoard.addEventListener('click',function(event) {
+    if(event.target.classList.contains('grid-item')) {
+        updateGameboard(event)
+        // switchTurns()
+        // checkBoard()
+     }
+})
 
 
 // FUNCTIONS
@@ -30,16 +52,19 @@ function createPlayer() {
     var playerOne = {
         id: 1,
         token: '🐝',
+        isTurn: true,
         wins: 0
     }
     var playerTwo = {
         id: 2,
         token: '🐻',
+        isTurn: false,
         wins: 0
     }
     players.push(playerOne, playerTwo)
 };
 
+// Display player data
 function displayWins() {
     playerOneWins.innerText = `${players[0].wins} wins`
     playerTwoWins.innerText = `${players[1].wins} wins`
@@ -64,11 +89,58 @@ function loadGame() {
 }
 
 // Increase Wins
+// if checkBoard() is playerOne, players[0].wins ++
+// if checkBoard() is playerTwo, players[1].wins ++
+
 // Keep track of gameboard data
-// Keep track of which player's turn it is
-// Check board for win conditions
-// Check board for a draw
+function updateGameboard(e) {
+    console.log(`updateGameboard`)
+    // console.log(e.srcElement.attributes[1].nodeValue)
+    gameBoardMoves.playerOneMoves.push(e.srcElement.attributes[1].nodeValue)
+    console.log('gameboardMoves', gameBoardMoves)
+    //if player.isTurn change the target's inner text to that players token
+}
+
+// Display gameboard data:
+// function displayMoves() {
+
+// }
+
+// function makeAnnouncement() {
+// update the announcement element's innerText to display whose turn it is
+// if someone has won, announce the winner, then timeout and display whose turn it is
+// if there is draw, announce there's a draw, then timeout and display whose turn it is
+// }
+
+// Keep track of which player's turn it is:
+function switchTurns() {
+    // turns ++
+    // Q: how will I keep track of who started the last round?
+
+    //check the player object to see if it's their turn
+    //if it is the players turn, reassign isTurn to false
+    //and assign the other players turn to true
+    
+    
+}
+
+// Check board for win conditions & check board for a draw:
+function checkBoard() {
+// declare winner variable
+// look through gameBoardMoves[0] and [1]
+// if gameboardMoves[0] includes a winning condition, player one assigned to winner
+// if gameboardMoves[1] includes a winning condition, player two to winner
+// if turns = 9 and neither array includes a winning condition, determine a draw
+// and winner assigned to null
+// return winner
+}
+
+
+
 // Reset gameboard and start new game
-// Display gameboard data
-// Display player data
+function endGame() {
+// reset gameBoardMoves
+}
+
+
 
