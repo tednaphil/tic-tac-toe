510b29631bace0665228639b203e0c1a67bbc885
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-01-11 15:17:16 -0600

    Remove console logs and unused code

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ var gameBoardMoves = {
     allMoves: []
 };
 var startedGame = 'playerOne'
-var turns = 0
 
 // EVENT LISTENERS
 addEventListener('load', loadGame);
@@ -36,14 +35,10 @@ gameBoard.addEventListener('click',function(event) {
         checkBoard()
         increaseWins()
      }
-})
-
+});
 
 // FUNCTIONS
-// Create a player:
-
 function createPlayer() {
-    // console.log('createPlayer')
     var playerOne = {
         id: 1,
         token: '🐝',
@@ -59,90 +54,31 @@ function createPlayer() {
     players.push(playerOne, playerTwo)
 };
 
-// Display player data
+function switchTurns() {
+    if (players[0].isTurn) {
+        players[1].isTurn = true
+        players[0].isTurn = false
+    } else {
+        players[0].isTurn = true
+        players[1].isTurn = false
+    }
+};
+
 function displayWins() {
     playerOneWins.innerText = `${players[0].wins} wins`
     playerTwoWins.innerText = `${players[1].wins} wins`
 };
 
-function clearBoard() {
-    for (var i = 0; i < 9; i++) {
-        boardSpaces[i].innerText = ''
-    }
-};
-
-function loadGame() {
-    createPlayer();
-    displayWins();
-    clearBoard();
-    announceTurn();
-};
-
-// Increase Wins:
-function increaseWins() {
-    // console.log('increase wins winner:', checkBoard())
-    if (checkBoard() === 'playerOne') {
-        players[0].wins ++
-        console.log('win for player one')
-        console.log(players[0].wins)
-        announceWinner(players[0])
-        disableBoard()
-        setTimeout(resetGame, 2500)
-        // resetGame()
-    }
-    if (checkBoard() === 'playerTwo') {
-        players[1].wins ++
-        console.log('win for player two')
-        console.log(players[1].wins)
-        announceWinner(players[1])
-        disableBoard()
-        setTimeout(resetGame, 2500)
-        // resetGame()
-    } else if (checkBoard() === null) {
-        console.log('twas a draw')
-        announceWinner('draw')
-        disableBoard()
-        setTimeout(resetGame, 2500)
-        // resetGame()
-    }
-    displayWins()
-}
-
-// Keep track of gameboard data
-function updateGameboard(e) {
-    // console.log(`updateGameboard`)
-    // console.log(e.srcElement.attributes[1].nodeValue)
-    var space = e.srcElement.attributes[1].nodeValue
-    if (players[0].isTurn && !gameBoardMoves.allMoves.includes(space)) {
-        gameBoardMoves.playerOneMoves.push(space);
-        gameBoardMoves.allMoves.push(space)
-        // switchTurns();
-    }
-    if (players[1].isTurn && !gameBoardMoves.allMoves.includes(space)) {
-        gameBoardMoves.playerTwoMoves.push(space);
-        gameBoardMoves.allMoves.push(space)
-        // switchTurns();
-    }
-    gameBoardMoves.playerOneMoves.sort()
-    gameBoardMoves.playerTwoMoves.sort()
-    // console.log(`player two sorted moves`, gameBoardMoves.playerTwoMoves)
-    // console.log('all moves', gameBoardMoves.allMoves)
-    // console.log('gameboardMoves', gameBoardMoves)
-}
-
-// Display gameboard data:
 function displayMoves() {
-    var playerOneMoves = gameBoardMoves.playerOneMoves;
-    var playerTwoMoves = gameBoardMoves.playerTwoMoves;
+    var playerOneMoves = gameBoardMoves.playerOneMoves
+    var playerTwoMoves = gameBoardMoves.playerTwoMoves
     for (var i = 0; i < playerOneMoves.length; i++) {
         boardSpaces[playerOneMoves[i]].innerText = `${players[0].token}`
-        // console.log(boardSpaces[playerOneMoves[i]])
     }
     for (var i = 0; i < playerTwoMoves.length; i++) {
         boardSpaces[playerTwoMoves[i]].innerText = `${players[1].token}`
-        // console.log(boardSpaces[playerTwoMoves[i]])
     }
-}
+};
 
 function announceTurn() {
     if (players[0].isTurn) {
@@ -151,34 +87,22 @@ function announceTurn() {
     if (players[1].isTurn) {
         announcement.innerText = `It's ${players[1].token}'s turn`
     }   
-}
+};
 
-function announceWinner(playerObject) {
-    console.log(`announceWinner`)
-    if (checkBoard()) {
-        announcement.innerText = `${playerObject.token} won the honey!`
-        console.log('winner announcement')
-    } else if (checkBoard() === null) {
-        announcement.innerText = `It's a draw!`
-        console.log('draw announcement')
+function updateGameboard(e) {
+    var space = e.srcElement.attributes[1].nodeValue
+    if (players[0].isTurn && !gameBoardMoves.allMoves.includes(space)) {
+        gameBoardMoves.playerOneMoves.push(space)
+        gameBoardMoves.allMoves.push(space)
     }
-}
-
-// Keep track of which player's turn it is:
-function switchTurns() {
-    turns ++
-    if (players[0].isTurn) {
-        players[1].isTurn = true;
-        players[0].isTurn = false;
-        console.log(`it's player two's turn next`)
-    } else {
-        players[0].isTurn = true;
-        players[1].isTurn = false;
-        console.log(`it's player one's turn next`)
+    if (players[1].isTurn && !gameBoardMoves.allMoves.includes(space)) {
+        gameBoardMoves.playerTwoMoves.push(space)
+        gameBoardMoves.allMoves.push(space)
     }
-}
+    gameBoardMoves.playerOneMoves.sort()
+    gameBoardMoves.playerTwoMoves.sort()
+};
 
-// Check board for win conditions & check board for a draw:
 function checkBoard() {
     var winningConditions = [
         [0, 1, 2],
@@ -189,91 +113,78 @@ function checkBoard() {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ];
-
-    // var winningConditionRegExps = [
-    //     /[012]/,
-    //     /[345]/,
-    //     /[678]/,
-    //     /[036]/,
-    //     /[147]/,
-    //     /[258]/,
-    //     /[048]/,
-    //     /[246]/
-    // ]
+    ]
     var winner = ''
-    // var testString = '2, 4, 5, 6'
-    // console.log('playerTwoMoves', gameBoardMoves.playerTwoMoves.toString())
-    // console.log('playerOneMoves', gameBoardMoves.playerOneMoves)
-    // console.log(gameBoardMoves.playerOneMoves.toString())
-    // console.log('all moves', gameBoardMoves.allMoves)
-    // console.log(winningConditions[7])
-    // console.log('regex test', /[246]/.test(testString))
-    // console.log('regexps array test', winningConditionRegExps[7].test(testString))
-    
-    // console.log('multisearch', gameBoardMoves.playerOneMoves.toString().includes(winningConditions[0][0])
-    // && gameBoardMoves.playerOneMoves.toString().includes(winningConditions[0][1])
-    // && gameBoardMoves.playerOneMoves.toString().includes(winningConditions[0][2]))
-    
-    // console.log('multi-search experiment', gameBoardMoves.playerOneMoves.toString().includes((winningConditions[0][0] && winningConditions[0][1] && winningConditions[0][2])))
-    console.log(winningConditions[0] in gameBoardMoves.playerOneMoves)
-    // console.log(['2', '4', '5', '6'].includes(winningConditions[7]))
-    // console.log(['2', '4', '5', '6'].toString().includes(winningConditions[7]))
-    // console.log(winningConditions[7])
-    // console.log(['2', '4', '5', '6'].toString())
-    // console.log([2, 4, 5, 6].includes(winningConditions[7]))
     for (var i = 0; i < winningConditions.length; i++) {
-        // console.log('checkBoard for loop')
-        // console.log('gameboard moves player One', gameBoardMoves.playerOneMoves.toString())
-        // console.log('winning condition 0', winningConditions[0].toString())
-        // console.log(gameBoardMoves.playerOneMoves.toString().includes(winningConditions[i]))
-        // console.log(gameBoardMoves.playerOneMoves.includes(winningConditions[i]))
-        // console.log(winningConditions[3])
-        // console.log('player one moves', gameBoardMoves.playerOneMoves.toString())
-        // console.log(gameBoardMoves.playerTwoMoves.toString().includes(winningConditions[i]))
-        
-        // if (winningConditionRegExps[i].test(gameBoardMoves.playerOneMoves.toString())) {
-        //     // console.log('the regex test worked')
-        //     console.log(winningConditionRegExps[0])
-        //     winner = 'playerOne'
-        //     console.log('player one wins')
-        // } else if (winningConditionRegExps[i].test(gameBoardMoves.playerTwoMoves.toString())) {
-        //     winner = 'playerTwo'
-        //     console.log('player two wins')
-        // } else if (gameBoardMoves.allMoves.length === 9 && winner === '') {
-        //     winner = null
-        //     console.log('a draw')
-        // }
-
         if (gameBoardMoves.playerOneMoves.toString().includes(winningConditions[i][0])
             && gameBoardMoves.playerOneMoves.toString().includes(winningConditions[i][1])
             && gameBoardMoves.playerOneMoves.toString().includes(winningConditions[i][2])) {
                 winner = 'playerOne'
-                console.log('player one wins')
         }
         else if (gameBoardMoves.playerTwoMoves.toString().includes(winningConditions[i][0])
                 && gameBoardMoves.playerTwoMoves.toString().includes(winningConditions[i][1])
                 && gameBoardMoves.playerTwoMoves.toString().includes(winningConditions[i][2])) {
                     winner = 'playerTwo'
-                    console.log('player two wins')
         }
         else if (gameBoardMoves.allMoves.length === 9 && winner === '') {
             winner = null
-            console.log('a draw')
         }
     }
     return winner
-}
+};
 
+function announceWinner(playerObject) {
+    if (checkBoard()) {
+        announcement.innerText = `${playerObject.token} won the honey!`
+    } else if (checkBoard() === null) {
+        announcement.innerText = `It's a draw!`
+    }
+};
 
-// Reset gameboard and start new game:
+function increaseWins() {
+    if (checkBoard() === 'playerOne') {
+        players[0].wins ++
+        announceWinner(players[0])
+        disableBoard()
+        setTimeout(resetGame, 2500)
+    }
+    if (checkBoard() === 'playerTwo') {
+        players[1].wins ++
+        announceWinner(players[1])
+        disableBoard()
+        setTimeout(resetGame, 2500)
+    } else if (checkBoard() === null) {
+        announceWinner('draw')
+        disableBoard()
+        setTimeout(resetGame, 2500)
+    }
+    displayWins()
+};
+
+function clearBoard() {
+    for (var i = 0; i < 9; i++) {
+        boardSpaces[i].innerText = ''
+    }
+};
+
+function disableBoard() {
+    for (var i = 0; i < 9; i++) {
+        boardSpaces[i].disabled = true
+    }
+};
+
+function enableBoard() {
+    for (var i = 0; i < 9; i++) {
+        boardSpaces[i].disabled = false
+    }
+};
+
 function resetGame() {
     gameBoardMoves = {
         playerOneMoves: [],
         playerTwoMoves: [],
         allMoves: []
-    };
-    console.log('started this game', startedGame)
+    }
     if (startedGame === 'playerOne') {
         startedGame = 'playerTwo'
         players[1].isTurn = true
@@ -283,23 +194,15 @@ function resetGame() {
         startedGame = 'playerOne'
         players[0].isTurn = true
         players[1].isTurn = false
-    };
-    turns = 0;
-    clearBoard();
+    }
+    clearBoard()
     enableBoard()
-    announceTurn();
-console.log('starting next game', startedGame)
-console.log(gameBoardMoves)
-}
+    announceTurn()
+};
 
-function disableBoard() {
-    for (var i = 0; i < 9; i++) {
-        boardSpaces[i].disabled = true;
-    }
-}
+function loadGame() {
+    createPlayer()
+    displayWins()
+    announceTurn()
+};
 
-function enableBoard() {
-    for (var i = 0; i < 9; i++) {
-        boardSpaces[i].disabled = false;
-    }
-}
